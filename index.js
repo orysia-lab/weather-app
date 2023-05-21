7cb6715e14964db4170895f3369c9dd1bfab1ed8
orysia-lab <iryndrobot@gmail.com>
2023-05-21 20:25:24 +0200

    version of vanilla

--- index.js
+++ index.js
@@ -25,54 +25,84 @@ p.innerHTML = `${day}, ${hour}:${minutes}`;
 
 let cityForm = document.querySelector("form");
 
+
 function showWeather(response) {
-  celsiusTemperature = response.data.main.temp
+   celsiusTemperature = response.data.main.temp
   let temperature = Math.round(response.data.main.temp);
   let showTemp = document.querySelector("#current-temp");
   showTemp.innerHTML = `${temperature}`;
   let changeCity = document.querySelector("#city");
   changeCity.innerHTML = response.data.name;
+
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
+
+
 
 function displayForecast(response) {
   let forecast = response.data.daily;
 
-  let forecastElement = document.querySelector("#weather-forecast");
+  let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="container text-center">`;
-  forecastHTML = forecastHTML + `<div class="row">`;
 
+//
+  let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
-      <div class="col-sm-12 col-md-4 col-lg-2">
-        <div class="day-card">
-          <h5>${formatDay(forecastDay.dt)}</h5>
-          <img src="img/${forecastDay.weather[0].icon}.png" alt="Weather" />
-          <p>
-            <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°C  </span>
-            <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°C</span>
-          </p>
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
         </div>
       </div>
-      `;
+  `;
     }
   });
 
   forecastHTML = forecastHTML + `</div>`;
-
   forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
+  let apiKey = "02c067fbe0a95f847d98a3fc4fe7414d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
-//
 
 function search(city){
   let apiKey = "02c067fbe0a95f847d98a3fc4fe7414d";
@@ -80,11 +110,14 @@ function search(city){
   axios.get(apiUrl).then(showWeather);
 }
 
-function searchCity(event) {
+function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  search(city)
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 function currentPosition(position) {
   let apiKey = "02c067fbe0a95f847d98a3fc4fe7414d";
@@ -100,33 +133,5 @@ function handlePosition(event) {
 }
 
 let currentButton = document.querySelector("#current-location");
-currentButton.addEventListener("click", handlePosition);
-
-
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#current-temp");
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
+currentButton.addEventListener("submit", handlePosition);
 search("Brugge");
